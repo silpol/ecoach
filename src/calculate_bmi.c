2d068a0d216a67cba932767d57633a53e1495704
Kai Skiftesvik <kaitsu>
2008-10-15 11:57:21 +0000

    Calculate BMI function structure (error)
    git-svn-id: https://vcs.maemo.org/svn/ecoach/trunk@11 96bd9ecb-e709-410d-80ab-3eb50248857c

---
+++ src/calculate_bmi.c
@@ -0,0 +1,45 @@
+/*
+ *  eCoach
+ *
+ *  Copyright (C) 2008  Kai Skiftesvik
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  See the file COPYING
+ */
+
+/*****************************************************************************
+ * Includes                                                                  *
+ *****************************************************************************/
+
+/* This module */
+#include "calculate_bmi.h"
+
+/* Other modules */
+#include "interface.h"
+
+/*---------------------------------------------------------------------------*
+ * Public functions                                                          *
+ *---------------------------------------------------------------------------*/
+
+void calculate_bmi_dialog_show(
+		NavigationMenu *menu,
+		GtkTreePath *path,
+		gpointer user_data)
+{
+   //   hildon_banner_show_information(GTK_WIDGET(window), NULL, "Hi there!");
+ 
+}
+
