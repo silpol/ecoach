6fb041f0ce9d859e8bccdfa8712ccf31a939cba5
Sampo Savola <sampp>
2008-10-30 09:52:17 +0000

    Modified to correspond needed headers
    git-svn-id: https://vcs.maemo.org/svn/ecoach/trunk@71 96bd9ecb-e709-410d-80ab-3eb50248857c

--- src/calculate_bmi.h
+++ src/calculate_bmi.h
@@ -1,50 +1,31 @@
 /*
- *  eCoach
- *
- *  Copyright (C) 2008  Kai Skiftesvik
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- *  See the file COPYING
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA  02110-1301, USA.
+
+    ---
+    Copyright (C) 2008, Sampo Savola, Kai Skiftesvik
  */
+
 
 #ifndef _CALCULATE_BMI_H
 #define _CALCULATE_BMI_H
-
-
-/*****************************************************************************
- * Includes                                                                  *
- *****************************************************************************/
-
-/* Configuration */
-#include "config.h"
-
-/* Gtk */
-#include <gtk/gtk.h>
-
-/* Other modules */
-#include "gconf_helper.h"
 #include "navigation_menu.h"
+#include <gtk/gtk.h>
+#include "interface.h"
 
-/*****************************************************************************
- * Function prototypes                                                       *
- *****************************************************************************/
-
-void calculate_bmi_dialog_show(
-		NavigationMenu *menu,
-		GtkTreePath *path,
-		gpointer user_data);
+void show_calculate_bmi(NavigationMenu *menu, GtkTreePath *path,
+				gpointer user_data);
 
 #endif /* _CALCULATE_BMI_H */
