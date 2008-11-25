ade00469bc32aee90515182e074800563b87b59c
Kai Skiftesvik <kaitsu>
2008-11-25 11:33:29 +0000

    added display_on setup
    git-svn-id: https://vcs.maemo.org/svn/ecoach/trunk@296 96bd9ecb-e709-410d-80ab-3eb50248857c

--- src/general_settings.c
+++ src/general_settings.c
@@ -21,27 +21,25 @@ Copyright (C) 2008, Sampo Savola, Kai Skiftesvik
 #include "gconf_helper.h"
 #include "gconf_keys.h"
 
-static GtkSpinButton *spinner;
-static GtkSpinButton *spinner2;
-static GtkLabel *val_label;
 void show_general_settings(NavigationMenu *menu, GtkTreePath *path,
 			gpointer user_data)
 {
 	
 	AppData *app_data = (AppData *)user_data;
 	GtkWidget *dialog;
-	GtkWidget *hbox;
-	GtkWidget *main_vbox;
-	GtkWidget *vbox;
-	GtkWidget *vbox2;
-	GtkWidget *radio1;
-	GtkWidget *radio2;
-	GtkWidget *entry;
+	GtkWidget *table;
+	GtkWidget *radio_units_metric;
+	GtkWidget *radio_units_english;
+	GtkWidget *radio_display_on;
+	GtkWidget *radio_display_off;
 	GtkWidget *label;
-	GtkAdjustment *adj;
-	GtkAdjustment *adj2;
 	GConfValue *value = NULL;
 	gint result;
+
+// for debugging only
+// gconf_helper_set_value_bool(app_data->gconf_helper, DISPLAY_ON, FALSE);
+
+
 	/* Create the widgets */
 	
 	dialog = gtk_dialog_new_with_buttons ("General Settings",
@@ -51,60 +49,74 @@ void show_general_settings(NavigationMenu *menu, GtkTreePath *path,
 						GTK_RESPONSE_OK,GTK_STOCK_CANCEL,
 						GTK_RESPONSE_REJECT,NULL);
 
-	gtk_widget_set_size_request(dialog, 300,150);
+	gtk_widget_set_size_request(dialog, 300,240);
 
-	main_vbox = gtk_vbox_new (FALSE, 2);
+	table = gtk_table_new (4, 2, TRUE);
+	gtk_table_set_homogeneous (GTK_TABLE (table), FALSE);
 	gtk_container_add (GTK_CONTAINER (GTK_DIALOG(dialog)->vbox),
-			main_vbox);
-
-	vbox = gtk_vbox_new (FALSE, 0);
-	gtk_container_set_border_width (GTK_CONTAINER (vbox), 2);
-	gtk_container_add (GTK_CONTAINER (main_vbox), vbox);
-
-	hbox = gtk_hbox_new (FALSE, 0);
-	gtk_box_pack_start (GTK_BOX (vbox), hbox, TRUE, TRUE, 2);
-
-	vbox2 = gtk_vbox_new (FALSE, 0);
-	gtk_box_pack_start (GTK_BOX (hbox), vbox2, TRUE, TRUE, 2);
+			table);
 
 	label = gtk_label_new ("Units:");
-	gtk_box_pack_start (GTK_BOX (vbox2), label, TRUE, TRUE, 2);
+	gtk_misc_set_alignment( GTK_MISC(label), 1.0, 0.5 );
+	gtk_table_attach_defaults(GTK_TABLE (table), label, 0, 1, 0, 1);
 
-	vbox2 = gtk_vbox_new (FALSE, 0);
-	gtk_box_pack_start (GTK_BOX (hbox), vbox2, TRUE, TRUE, 2);
-
-	radio1 = gtk_radio_button_new_with_label (NULL, "Metric");
+	radio_units_metric = gtk_radio_button_new_with_label (NULL, "Metric");
+	gtk_table_attach_defaults(GTK_TABLE (table), radio_units_metric, 1, 2, 0, 1);
 	
-	gtk_box_pack_start (GTK_BOX (vbox2), radio1, TRUE, TRUE, 2);
+	radio_units_english = gtk_radio_button_new_with_label_from_widget (GTK_RADIO_BUTTON 
+							(radio_units_metric), "English");
+	gtk_table_attach_defaults(GTK_TABLE (table), radio_units_english, 1, 2, 1, 2);
 
-	radio2 = gtk_radio_button_new_with_label_from_widget (GTK_RADIO_BUTTON (radio1),
-							"English");
-	gtk_box_pack_start (GTK_BOX (vbox2), radio2, TRUE, TRUE, 2);
-	
-	/*Check which radio  button should be set */
-
+	/*Check which unit-radio button should be set */
 	if(gconf_helper_get_value_bool_with_default(app_data->gconf_helper,
-						    USE_METRIC,TRUE))
-	{
-	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (radio1), TRUE);
+						    USE_METRIC,TRUE)) {
+		gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (radio_units_metric), TRUE);
+	} else {
+		gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (radio_units_english), TRUE);
 	}
-	else
-	{
-	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (radio2), TRUE);
+
+	label = gtk_label_new ("Display:");
+	gtk_misc_set_alignment( GTK_MISC(label), 1.0, 0.5 );
+	gtk_table_attach_defaults(GTK_TABLE (table), label, 0, 1, 2, 3);
+	
+	radio_display_on = gtk_radio_button_new_with_label (NULL, "On");
+	gtk_table_attach_defaults(GTK_TABLE (table), radio_display_on, 1, 2, 2, 3);
+
+	radio_display_off = gtk_radio_button_new_with_label_from_widget (GTK_RADIO_BUTTON 
+							(radio_display_on), "Off");
+	gtk_table_attach_defaults(GTK_TABLE (table), radio_display_off, 1, 2, 3, 4);
+
+	/*Check which display dimm-radio button should be set */
+	if(gconf_helper_get_value_bool_with_default(app_data->gconf_helper,
+						    DISPLAY_ON,TRUE)) {
+		gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (radio_display_on), TRUE);
+	} else {
+		gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (radio_display_off), TRUE);
 	}
+
 	gtk_widget_show_all (dialog);
 	result = gtk_dialog_run(GTK_DIALOG(dialog));
 	if(result == GTK_RESPONSE_OK)
 	{
-		if(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(radio1)))
+		if(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(radio_units_metric)))
 		{
 			gconf_helper_set_value_bool(app_data->gconf_helper,
 						    USE_METRIC,TRUE);
 		}
-		if(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(radio2)))
+		if(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(radio_units_english)))
 		{
 			gconf_helper_set_value_bool(app_data->gconf_helper,
 					USE_METRIC,FALSE);
+		}
+		if(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(radio_display_on)))
+		{
+			gconf_helper_set_value_bool(app_data->gconf_helper,
+					DISPLAY_ON,TRUE);
+		}
+		if(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(radio_display_off)))
+		{
+			gconf_helper_set_value_bool(app_data->gconf_helper,
+					DISPLAY_ON,FALSE);
 		}
 		gtk_widget_destroy(dialog);
 	}
@@ -112,4 +124,6 @@ void show_general_settings(NavigationMenu *menu, GtkTreePath *path,
 	{
 		gtk_widget_destroy(dialog);	
 	}
+
+
 }
