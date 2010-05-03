c9a645ed60bc424fdd2d904451c497c046c0aa04
Sampo Savola <sampp>
2010-05-03 13:50:33 +0000

    updated version number
    git-svn-id: https://vcs.maemo.org/svn/ecoach/trunk@679 96bd9ecb-e709-410d-80ab-3eb50248857c

--- src/map_view.cc
+++ src/map_view.cc
@@ -2148,7 +2148,7 @@ static void about_dlg(HildonButton *button, gpointer user_data){
     gtk_show_about_dialog((GtkWindow*)self->win
                 , "name",      "eCoach"
 		, "logo-icon-name",	"ecoach"
-                , "version",   "1.56"
+                , "version",   "1.60"
                 , "copyright", "Sampo Savola <samposav@paju.oulu.fi>\n"
 		"Jukka Alasalmi\n Veli-Pekka Haajanen\n Kai Skiftesvik"
                 , "website",   "http://ecoach.garage.maemo.org"
